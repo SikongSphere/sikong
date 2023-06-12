c3cd47986eb0b7bd50ab0bdf7a1d57e63d235034
achaochao <liangc0427@gmail.com>
2023-06-12 23:57:03 +0800

    ignore repo

--- repo/LMFlow/src/lmflow/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-"""This Python code defines a class Dataset with methods for initializing, loading,
-and manipulating datasets from different backends such as Hugging Face and JSON.
- 
-The `Dataset` class includes methods for loading datasets from a dictionary and a Hugging
-Face dataset, mapping datasets, and retrieving the backend dataset and arguments.
-"""
-from lmflow.datasets.dataset import Dataset
